5e02babc034fc2bddd7bf8150715410dc815b092
Khumoyun Khujamov <xojamov07@gmail.com>
2020-05-10 14:57:40 +0500

    fixed some bugs in Back-end, configured CORS

--- back-end/app/User.php
+++ back-end/app/User.php
@@ -46,4 +46,10 @@ class User extends Authenticatable implements JWTSubject
     {
         return [];
     }
+    public function setPasswordAttribute($password)
+    {
+        if ( !empty($password) ) {
+            $this->attributes['password'] = bcrypt($password);
+        }
+    }
 }
